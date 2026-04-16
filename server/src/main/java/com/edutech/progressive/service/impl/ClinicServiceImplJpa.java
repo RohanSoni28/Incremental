37cf576cdf7cc42e5f7bcf0dd0bdc3d1f8f78346
RohanSoni28 <dummy@gmail.com>
2026-04-16 14:26:06 +0000

    Day 10 Done

--- server/src/main/java/com/edutech/progressive/service/impl/ClinicServiceImplJpa.java
+++ server/src/main/java/com/edutech/progressive/service/impl/ClinicServiceImplJpa.java
@@ -24,6 +24,7 @@ public class ClinicServiceImplJpa implements ClinicService {
         return clinicRepository.findAll();
 
     }
+    
 
     @Override
     public Clinic getClinicById(int clinicId) throws SQLException {
